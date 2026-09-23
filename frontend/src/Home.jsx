b73cfbb1bbdd8de4e37b2aa2996bbb53309ddb98
AlexandruPopaFabian <alexandru.popa12@e-uvt.ro>
2026-09-24 01:28:45 +0300

    Delete frontend directory

--- frontend/src/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Home() {
-    return (
-        <div className="app">
-            <h1>Welocome to GymFit !</h1>
-        </div>);
-}
-export default Home;
--- frontend/src/api/members.js
+++
@@ -1,9 +0,0 @@
-import { apiGet } from "./client";
-
-export function getMembers() {
-    return apiGet("/Members");
-}
-
-export function getMember(id) {
-    return apiGet(`/Members(${id})`);
-}
